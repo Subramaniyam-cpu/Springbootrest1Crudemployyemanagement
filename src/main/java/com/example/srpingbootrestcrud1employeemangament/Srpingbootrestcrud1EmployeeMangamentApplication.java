7e820e60ef1470d321ebd6dbc73210591dd16d32
Subramaniyam-cpu <2022cfse004@wilp.bits-pilani.ac.in>
2023-03-19 06:06:05 -0800

    Intial setup

---
+++ src/main/java/com/example/srpingbootrestcrud1employeemangament/Srpingbootrestcrud1EmployeeMangamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.srpingbootrestcrud1employeemangament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Srpingbootrestcrud1EmployeeMangamentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Srpingbootrestcrud1EmployeeMangamentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/srpingbootrestcrud1employeemangament/Srpingbootrestcrud1EmployeeMangamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srpingbootrestcrud1employeemangament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Srpingbootrestcrud1EmployeeMangamentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
